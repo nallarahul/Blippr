db4c80b7a4f19e9714cb5c7bbda720d495cdb84c
nallarahul <nallarahu515@gmail.com>
2025-04-28 00:06:31 +0530

    added login and logout API

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -7,16 +7,16 @@ export const signup = async (req,res)=>{
     try {
 
         if(!fullName || !email){
-            return(res.status(400).json({ message: "All fields are required !!" }))
+            return(res.status(400).json({ message: "All fields are required." }))
         }
 
         if(password.length < 6){
-            return(res.status(400).json({ message: "Password must contain atleast 6 characters" }))
+            return(res.status(400).json({ message: "Password must contain atleast 6 characters." }))
         }
 
         const user = await User.findOne({email});
 
-        if(user){return(res.status(400).json({ message: "Email already exists" }))}
+        if(user){return(res.status(400).json({ message: "Email already exists." }))}
 
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password,salt);
@@ -45,10 +45,35 @@ export const signup = async (req,res)=>{
     }
 };
 
-export const login = (req,res)=>{
-    res.send("signup route");
+export const login = async (req,res)=>{
+    const {email,password} = req.body;
+    try {
+        const user = await User.findOne({email});
+        if(!user)
+            return(res.status(400).json({ message: "Invalid Credentials" }));
+        const isPasswordCorrect = await bcrypt.compare(password,user.password);
+        if(!isPasswordCorrect)
+                return(res.status(400).json({ message: "Invalid Credentials" }));
+        generateToken(user._id,res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic
+        });
+    } catch (error) {
+        console.log("Error in Sign up controller ", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 };
 
 export const logout = (req,res)=>{
-    res.send("signup route");
+    try {
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({ message: "Logged out successfully !!" });
+    } catch (error) {
+        console.log("Error in Sign up controller ", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 };
