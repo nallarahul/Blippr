324b5ab8b179ff994f46611d89f0438582c2cb74
nallarahul <nallarahu515@gmail.com>
2025-05-19 17:14:58 +0530

    deployment configurations done

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,11 @@ import { connectDB } from './lib/db.js';
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app,server } from './lib/socket.js';
+import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
@@ -19,6 +21,13 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 
 server.listen(PORT, () =>{
     console.log("App running on PORT: "+PORT);
