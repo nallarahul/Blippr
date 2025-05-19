874eb96d2fcae13d00425d3a9ae440a9768c54e1
nallarahul <nallarahu515@gmail.com>
2025-05-19 19:20:34 +0530

    made changes in message routes

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -5,7 +5,7 @@ import { getUsersForSideBar, getMessages, sendMessages } from "../controllers/me
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSideBar);
+router.get("/chat/:id", protectRoute, getMessages);
 router.post("/send/:id",protectRoute, sendMessages);
-router.get("/:id",protectRoute,getMessages);
 
 export default router;
